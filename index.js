e7728646076f2b57bac3eb4256d82ace26ea1eaf
Monique Lena Moore <moniquelmoore06@gmail.com>
2022-03-27 14:09:34 -0400

    1st task glichty?

---
+++ index.js
@@ -0,0 +1,9 @@
+//produces  error: AssertionError: expected NaN to equal 20
+//const divide = (p1,p2) => p1/p2; divide (2000,100)//this code returns correctly in repl
+//per repl divide() returns "NaN"
+
+const divide = (d,e) => d/e; divide(2000,100)
+
+const square = p => p*p; square(2)
+
+const add = (a,b) => a+b; add(3,4)
